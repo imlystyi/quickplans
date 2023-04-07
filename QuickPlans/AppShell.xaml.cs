417c6982715502dc1e3febd46129dec427322508
music-soul1-1 <72669184+music-soul1-1@users.noreply.github.com>
2023-04-07 15:43:51 +0300

    dark mode fix
    Dark mode style changed. Some light mode tweaks.

--- QuickPlans/AppShell.xaml.cs
+++ QuickPlans/AppShell.xaml.cs
@@ -9,11 +9,5 @@ public partial class AppShell : Shell
         Routing.RegisterRoute(nameof(Views.NotesCollectionPage), typeof(Views.NotesCollectionPage));
 
         Routing.RegisterRoute(nameof(Views.ToDoTasksCollectionPage), typeof(Views.ToDoTasksCollectionPage));
-
-        // Sets the white status bar to match the color of the application.
-#if ANDROID        
-        statusBarBehavior.StatusBarColor = Colors.White;
-        statusBarBehavior.StatusBarStyle = CommunityToolkit.Maui.Core.StatusBarStyle.DarkContent;
-#endif
     }
 }
